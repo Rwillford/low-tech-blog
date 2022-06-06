b2e8788057db1d0add8d1e58f8beed224f361ed2
Bob <rwillford0719@gmail.com>
2022-06-06 08:07:02 -0500

    added info into server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers })
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: '',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(require('./controllers/'));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now live on the Low Tech Blog!'));
+});
+
+
