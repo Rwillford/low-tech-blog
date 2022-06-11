bed755cd4e7637236da755f050f06a1755e82397
Bob <rwillford0719@gmail.com>
2022-06-11 11:00:30 -0500

    asdfsda

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 //create connection to the database
-const sequelize = process.env.JAWASDB_URL
-  ? new Sequelize(process.env.JAWASDB_URL)
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_Name, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
