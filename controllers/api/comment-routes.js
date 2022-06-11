298cc83ddbf92f5f452d879880d54bb16af9ef38
Bob <rwillford0719@gmail.com>
2022-06-10 19:10:51 -0500

    need to commit added handlebar routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,9 +2,49 @@ const router = require('express').Router();
 const {Comment } = require('../../models');
 
 //Get All comment
+router.get('/', (req, res) => {
+    Comment.findAll()
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 //Create Comment
+router.post('/', (req, res) => {
+    if(req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            user_id: req.session.user_id,
+            post_id: req.body.post_id
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
+});
 
 //Delete Comment
+router.delete('/:id', (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCommentData => {
+        if(!dbCommentData) {
+            res.status(404).json({ message: 'No comment found with this ID!' });
+            return;
+        }
+        res.json(dbCommentData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
