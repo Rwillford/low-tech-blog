44e66024d61e485e1e7d11f8d7700bd7d8cd8d5a
Bob <rwillford0719@gmail.com>
2022-06-11 09:22:03 -0500

    fixed more code

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/'));
+app.use(require('./controllers'));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
