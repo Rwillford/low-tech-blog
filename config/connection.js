183a06a822c28cb1de25cefc3b13e4ee5edb8a30
Bob <rwillford0719@gmail.com>
2022-06-11 09:14:17 -0500

    fixed code

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 //create connection to the database
-const sequelize = porcess.env.JAWASDB_URL
+const sequelize = process.env.JAWASDB_URL
   ? new Sequelize(process.env.JAWASDB_URL)
   : new Sequelize(process.env.DB_Name, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
