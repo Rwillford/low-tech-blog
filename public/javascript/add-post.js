22a62f9d16804d7675fc29b18c7f0cb25847aa48
Bob <rwillford0719@gmail.com>
2022-06-11 21:09:38 -0500

    one day it will end, but is not that day. what was i kidding, it never ends

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,13 +1,13 @@
 async function newFormHandler(event) {
     event.preventDefault();
 
-    const title = document.querySelector('input[name="post-title"]').value;
+    const post_title = document.querySelector('input[name="post_title"]').value;
     const post_text = document.querySelector('input[name="post_text"]').value;
 
     const response = await fetch(`/api/posts`, {
         method: 'POST',
         body: JSON.stringify({
-            title,
+            post_title,
             post_text
         }),
         headers: {
