016ba57ab92ac9e906632f4ad81fe40cb94c5a80
Bob <rwillford0719@gmail.com>
2022-06-12 19:47:26 -0500

    the hope is up that this is the last commit

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -4,7 +4,7 @@ async function newFormHandler(event) {
     const post_title = document.querySelector('input[name="post_title"]').value;
     const post_text = document.querySelector('input[name="post_text"]').value;
 
-    const response = await fetch(`/api/posts`, {
+    const response = await fetch('/api/posts', {
         method: 'POST',
         body: JSON.stringify({
             post_title,
